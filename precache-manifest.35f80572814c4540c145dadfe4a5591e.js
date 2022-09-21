34967aa2c74eab398e63b3bdd365d2a850c687c2
BasilDhilloo <bdhilloo@gmail.com>
2022-09-21 14:13:33 -0400

    Updates

---
+++ precache-manifest.35f80572814c4540c145dadfe4a5591e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a56e9dd5340db3a568d4eb2810a5e2",
+    "url": "/luci/index.html"
+  },
+  {
+    "revision": "caf1c8b771cf35184514",
+    "url": "/luci/static/css/2.fbab7092.chunk.css"
+  },
+  {
+    "revision": "8bfafd10fa35daec820f",
+    "url": "/luci/static/css/main.5275dc49.chunk.css"
+  },
+  {
+    "revision": "caf1c8b771cf35184514",
+    "url": "/luci/static/js/2.003ba200.chunk.js"
+  },
+  {
+    "revision": "b080a64e38a22f660fbc1d42d9c5f1e7",
+    "url": "/luci/static/js/2.003ba200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bfafd10fa35daec820f",
+    "url": "/luci/static/js/main.c2f14522.chunk.js"
+  },
+  {
+    "revision": "c1bed0683137329f5c0e",
+    "url": "/luci/static/js/runtime-main.8d1e60a3.js"
+  },
+  {
+    "revision": "4329bc5476730c77d8444be70d126c01",
+    "url": "/luci/static/media/admin_login_body_image.4329bc54.png"
+  },
+  {
+    "revision": "af6f809636ee00c7d12c3da11c30eff6",
+    "url": "/luci/static/media/background-2-m.af6f8096.jpg"
+  },
+  {
+    "revision": "945aaf6972e8dfad40935c5efa4b052c",
+    "url": "/luci/static/media/background-2.945aaf69.jpeg"
+  },
+  {
+    "revision": "1f302f20f1085649406975661ba023e4",
+    "url": "/luci/static/media/background.1f302f20.jpeg"
+  },
+  {
+    "revision": "f35843817b8a937ac9e554d2301fd674",
+    "url": "/luci/static/media/calimage.f3584381.png"
+  },
+  {
+    "revision": "5889e667b743273ebfdaba799b948f55",
+    "url": "/luci/static/media/contactus_body_image.5889e667.png"
+  },
+  {
+    "revision": "ed34bcc5451a5af0178ba548db441b3f",
+    "url": "/luci/static/media/contactus_jumbotron_image.ed34bcc5.png"
+  },
+  {
+    "revision": "08c914094efa3779d2066604b57315c4",
+    "url": "/luci/static/media/payment_details_body_image.08c91409.png"
+  },
+  {
+    "revision": "e88ae68f902ad578b5611d3e9c9a8550",
+    "url": "/luci/static/media/profile_image_with_camera_icon.e88ae68f.png"
+  },
+  {
+    "revision": "215a21c9fdb2a2739a4cf159f54c1f06",
+    "url": "/luci/static/media/team.215a21c9.png"
+  }
+]);
